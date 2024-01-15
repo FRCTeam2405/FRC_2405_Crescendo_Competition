4f1fdd23661fa28d46f5cb1f0cbbc2fa70f5744d
frc2405-programmer <2405.sk07@proton.me>
2024-01-15 18:49:55 -0500

    Import YAGSL and add swerve drive stubs

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public final class Constants {
+    public static final class Swerve {
+        /** Swerve drive max speed, in meters per second. */
+        public static final double MAX_SPEED = 4.0;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveContainer.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+// TODO! Configure the json files in deploy/swerve
+
+package frc.robot.subsystems;
+
+import java.io.File;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Constants;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
+public class SwerveContainer implements Subsystem {
+
+  public static SwerveDrive inner;
+
+  /** Creates a new SwerveContainer. */
+  public SwerveContainer() {
+    // Set the verbosity before initializing the swerve drive.
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+
+    // Try to init the swerve drive, and send an error if it fails.
+    try {
+      inner = new SwerveParser(new File(Filesystem.getDeployDirectory(), "swerve"))
+        .createSwerveDrive(Constants.Swerve.MAX_SPEED);      
+    } catch(Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+}
