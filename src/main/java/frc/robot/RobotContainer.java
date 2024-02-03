9c3606fba2dcf0f386a27d266cd4be7b52d39e54
frc2405-programmer <120429100+Temptingheart97@users.noreply.github.com>
2024-02-03 09:31:07 -0500

    Finished Small Rotating Square Auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -74,7 +74,8 @@ public class RobotContainer {
     autonChooser.addOption("Small Circle Test", new PathPlannerAuto("Small Circle Test Auto"));
     autonChooser.addOption("Small Square Test", new PathPlannerAuto("Small Square Auto"));
     autonChooser.addOption("Backwards", new PathPlannerAuto("Backwards")); 
-    autonChooser.addOption("Blue1", new PathPlannerAuto("collect3Blue1")); 
+    autonChooser.addOption("Blue1", new PathPlannerAuto("collect3Blue1"));
+    autonChooser.addOption("Small square test rotate", new PathPlannerAuto("Small Rotating Square")); 
 
     SmartDashboard.putData("autonDropdown", autonChooser);
   }
