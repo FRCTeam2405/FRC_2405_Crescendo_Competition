f9c05f908b427b8a614f0d23fae9beab0db52374
frc2405-programmer <2405.sk07@proton.me>
2024-02-24 14:01:55 -0500

    test pixy command

---
+++ src/main/java/frc/robot/commands/Pixy2TestCommand.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.pixy2_library.Pixy2CCC.Block;
+import frc.robot.subsystems.PixyCam;
+
+public class Pixy2TestCommand extends Command {
+
+  private PixyCam pixyCam;
+
+  /** Creates a new Pixy2TestCommand. */
+  public Pixy2TestCommand(PixyCam pixyCam) {
+    this.pixyCam = pixyCam;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(pixyCam);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    pixyCam.initialize();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    ArrayList<Block> blocks = pixyCam.getBlocks();
+    SmartDashboard.putNumber("firstBlockX", blocks.get(0).getX());
+    SmartDashboard.putNumber("firstBlockY", blocks.get(0).getY());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
