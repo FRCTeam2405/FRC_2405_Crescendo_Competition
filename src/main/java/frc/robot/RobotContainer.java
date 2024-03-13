1dc1cb66aeef69d3e9aa15c7b72636a2eb37828a
frc2405-programmer <2405.sk07@proton.me>
2024-03-13 18:16:08 -0400

    force test auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -186,20 +186,21 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-   if (sysDashboard.getAutonChooser().getSelected() != null && testAutonChooser.getSelected() != null) {
-    Commands.print("2 autonomous commands selected");
-    return null;
-   } else {
-    if (testAutonChooser.getSelected() != null) {
-     return testAutonChooser.getSelected();
-    } else {
-     if (sysDashboard.getAutonChooser().getSelected() != null) {
-      return sysDashboard.getAutonChooser().getSelected();
-     } else {
-      Commands.print("No autonomous commands selected");
-      return null;
-     }
-    }
-   }
+    return testAutonChooser.getSelected();
+  //  if (sysDashboard.getAutonChooser().getSelected() != null && testAutonChooser.getSelected() != null) {
+  //   Commands.print("2 autonomous commands selected");
+  //   return null;
+  //  } else {
+  //   if (testAutonChooser.getSelected() != null) {
+  //    return testAutonChooser.getSelected();
+  //   } else {
+  //    if (sysDashboard.getAutonChooser().getSelected() != null) {
+  //     return sysDashboard.getAutonChooser().getSelected();
+  //    } else {
+  //     Commands.print("No autonomous commands selected");
+  //     return null;
+  //    }
+  //   }
+  //  }
   }
 }
