904011b5cb42b9a3a694737f518b8dff7d28e3b1
frc2405-programmer <2405.sk07@proton.me>
2024-03-23 11:31:06 -0400

    enable isfinished

--- src/main/java/frc/robot/commands/swerve/SpeakerAimingDrive.java
+++ src/main/java/frc/robot/commands/swerve/SpeakerAimingDrive.java
@@ -155,12 +155,12 @@ public class SpeakerAimingDrive extends Command {
   // Returns true when the command should end during autonomous.
   @Override
   public boolean isFinished() {
-    return true;
+    // return true;
 
-    // if (DriverStation.isAutonomousEnabled() && Math.abs(swerveDrive.getYaw().getDegrees() - desiredYaw.getDegrees()) > 0.25) {
-    //   return true;
-    // } else {
-    //   return false;
-    // }
+    if (DriverStation.isAutonomousEnabled() && Math.abs(swerveDrive.getYaw().getDegrees() - desiredYaw.getDegrees()) > 0.25) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
